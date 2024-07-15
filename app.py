b1bd2b742fc3bd57f8309f7d6848a66cb70b1742
stevej <jatsasteve@gmail.com>
2024-07-15 12:47:00 +0200

    initial 2

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
